2cec6ce90141c96cb4d14bdbc7a3da21cee3f013
Cause Chung <cuzfrog@139.com>
2016-07-22 11:48:42 +0800

    implementing client methods

--- client/src/main/scala/com/github/cuzfrog/webdriver/WebDriverClient.scala
+++ client/src/main/scala/com/github/cuzfrog/webdriver/WebDriverClient.scala
@@ -7,6 +7,7 @@ import akka.io.IO
 import akka.pattern.ask
 import akka.util.Timeout
 import boopickle.Default._
+import com.github.cuzfrog.webdriver.Messages._
 import spray.can.Http
 import spray.http._
 
@@ -16,19 +17,30 @@ import scala.language.postfixOps
 
 object WebDriverClient {
 
-  import Messages._
+  def retrieveDriver(host: String, name: String): Driver = ask(RetrieveDriver(name))(host)
+  def newDriver(name: String, host: String, typ: DriverTypes.DriverType): Driver = ask(NewDriver(name, typ))(host)
+  implicit class ClientDriver(driver: Driver) {
+    def getWindows: Seq[Window] = ask(GetWindows(driver))
+    def getWindow: Window = ask(GetWindow(driver))
+    /**
+      * Kill driver on the server, and clean all associated elements in repository. Aka invoke WebDriver.quit().
+      */
+    def kill(): Unit = ask(Kill(driver))
+    /**
+      * Clean all associated elements in repository
+      * @return number of elements cleaned.
+      */
+    def clean(): Long = ask(Clean(driver))
+  }
 
-  def retrieve(host: String, name: String): Driver = ask(RetrieveDriver(name))(host)
-
-  def create(name: String, host: String, typ: DriverTypes.DriverType): Driver = ask(NewDriver(name,typ))(host)
 
   private[webdriver] implicit val system: ActorSystem = ActorSystem("WebDriverCli")
   private[webdriver] implicit val timeout: Timeout = Timeout(15 seconds)
 
   // implicit execution context
-  private[webdriver] def ask[T](message: Messages.Message)(implicit host: String): T =try {
+  private[webdriver] def ask[T](message: Messages.Message)(implicit host: String): T = try {
     val data = Pickle.intoBytes(message)
-    val response=(IO(Http) ? HttpRequest(method = HttpMethods.POST, uri = Uri(s"$host/tell"), entity = HttpEntity(data.array()))).mapTo[HttpResponse]
+    val response = (IO(Http) ? HttpRequest(method = HttpMethods.POST, uri = Uri(s"$host/tell"), entity = HttpEntity(data.array()))).mapTo[HttpResponse]
     val result = Await.result(response, 15 seconds)
     Unpickle[T].fromBytes(ByteBuffer.wrap(result.entity.data.toByteArray))
   }
--- shared/src/main/scala/com/github/cuzfrog/webdriver/Driver.scala
+++ shared/src/main/scala/com/github/cuzfrog/webdriver/Driver.scala
@@ -1,19 +1,3 @@
 package com.github.cuzfrog.webdriver
 
-case class Driver(_id: Long, name: String) {
-
-  def getWindows: Seq[Window] = ???
-
-  def getWindow:Window = ???
-
-  /**
-    * Kill driver on the server, and clean all associated elements in repository. Aka invoke WebDriver.quit().
-    */
-  def kill(): Unit = ???
-  /**
-    * Clean all associated elements in repository
- *
-    * @return number of elements cleaned.
-    */
-  def clean(): Long = ???
-}
+case class Driver(_id: Long, name: String)
