b32365857a85553f4e60983cad8169d7122e1246
cuzfrog <cuzfrog@139.com>
2016-07-24 09:04:42 +0800

    add docs

--- client/src/main/scala/com/github/cuzfrog/webdriver/AddClientMethod.scala
+++ client/src/main/scala/com/github/cuzfrog/webdriver/AddClientMethod.scala
@@ -37,8 +37,21 @@ case class ClientDriver(driver: Driver, private implicit val host: String) exten
 trait FindElementMethod {
   protected val _id: Long
   protected val host: String
+  /**
+    * Invoke FindElement on the server and return a stub of an element.
+    * If this element is a frame, methods are invoked after an automatic driver switch.
+    * @param attr which includes:id, name, tag, xpath, class/className, css/cssSelector, link and partialLink. Case insensitive.
+    * @param value of the attr.
+    * @return An Option of an element.
+    */
   def findElement(attr: String, value: String): Option[ClientElement] =
     ask[Ready[Element]](FindElement(_id, attr, value)).map(r => ClientElement(r.data, host))
+  /**
+    * Invoke associated method on the server and return a sequence of stubs.
+    * @param attr which includes:id, name, tag, xpath, class/className, css/cssSelector, link and partialLink. Case insensitive.
+    * @param value of the attr.
+    * @return An Seq of elements, when empty it be Nil.
+    */
   def findElements(attr: String, value: String): Seq[ClientElement] = {
     ask[Ready[Seq[Element]]](FindElements(_id, attr, value)).map(_.data.map(ClientElement(_, host))) match {
       case Some(s) => s
@@ -48,17 +61,36 @@ trait FindElementMethod {
 }
 case class ClientWindow(window: Window, host: String) extends FindElementMethod {
   val title = window.title
+  /**
+    * Equal with windowHandle of the driver on server.
+    */
   val handle = window.handle
   protected val _id = window._id
 }
 
 case class ClientElement(element: Element, host: String) extends FindElementMethod with LazyLogging {
-  val _id = element._id
+  protected val _id = element._id
 
+  /**
+    * Send keys to the element. May fail.
+    * @param keys
+    */
   def sendKeys(keys: String): Unit = ask[Success](SendKeys(element, keys)).foreach(f => logger.debug(f.msg))
+  /**
+    * If this causes a web reload or refresh, reference to elements before this invocation will be invalid.
+    */
   def submit(): Unit = ask[Success](Submit(element)).foreach(f => logger.debug(f.msg))
+  /**
+    * If this causes a web reload or refresh, reference to elements before this invocation will be invalid.
+    */
   def click(): Unit = ask[Success](Click(element)).foreach(f => logger.debug(f.msg))
+  /**
+    * Get value of the attribute.
+    */
   def getAttr(attr: String): Option[String] = ask[Success](GetAttr(element, attr)).map(_.msg)
+  /**
+    * Get text of this element.
+    */
   def getText:Option[String] = ask[Success](GetText(element)).map(_.msg)
 
 }
