62a8a031e6eaa58e3f6a723e1b52da5b566ebf82
Cause chung <cuzfrog@139.com>
2016-07-28 17:11:18 +0800

    server connection time out exception logging

--- client/src/main/scala/com/github/cuzfrog/webdriver/WebDriverClient.scala
+++ client/src/main/scala/com/github/cuzfrog/webdriver/WebDriverClient.scala
@@ -5,16 +5,16 @@ import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, TimeoutException}
 import scala.concurrent.duration.DurationInt
 import scala.language.postfixOps
 
 
 object WebDriverClient extends AddClientMethod with LazyLogging {
-  private val config=ConfigFactory.load
-  private val host=config.getString("webdriver.client.host")
-  private val timeoutSec=config.getInt("webdriver.client.timeout")
-  private val actionIntervalMs=config.getInt("webdriver.client.action-interval")
+  private val config = ConfigFactory.load
+  private val host = config.getString("webdriver.client.host")
+  private val timeoutSec = config.getInt("webdriver.client.timeout")
+  private val actionIntervalMs = config.getInt("webdriver.client.action-interval")
   private implicit val system: ActorSystem = ActorSystem("WebDriverCli")
   private implicit val timeout: Timeout = Timeout(timeoutSec seconds)
   private val remoteListener = system.actorSelection(s"akka.tcp://WebDriverServ@$host/user/handler")
@@ -34,6 +34,9 @@ object WebDriverClient extends AddClientMethod with LazyLogging {
     }
 
   } catch {
+    case e: TimeoutException=>
+      logger.debug("Server connection time out.")
+      None
     case e: Exception =>
       logger.debug(e.getMessage)
       None
--- client/src/test/scala/anywhere/WebDriverClientTest.scala
+++ client/src/test/scala/anywhere/WebDriverClientTest.scala
@@ -5,21 +5,22 @@ import com.github.cuzfrog.webdriver.{DriverTypes, WebDriverClient}
 import scala.language.implicitConversions
 
 object WebDriverClientTest extends App {
-  val host = "192.168.56.101:60001"
-  val driverName="test1"
-  val client=new WebDriverClient()
-  val driver =client.retrieveDriver(host, driverName) match {
-    case s@Some(_) => s
-    case None => client.newDriver(host, driverName, DriverTypes.Chrome)
+  val driverName = "test1"
+  try {
+    val driver = WebDriverClient.retrieveDriver(driverName) match {
+      case s@Some(_) => s
+      case None => WebDriverClient.newDriver(driverName, DriverTypes.Chrome)
+    }
+
+    implicit def getOption[T](option: Option[T]): T = option.get
+    val window = driver.navigateTo("http://www.bing.com")
+    window.findElement("id", "sb_form_q").sendKeys("Juno mission")
+    window.findElement("id", "sb_form").submit()
+    window.executeJS("console.log('testJS')")
+
+    scala.io.StdIn.readLine("press any to shut down the client.....")
+  } finally {
+    WebDriverClient.shutdownClient()
   }
-
-  implicit def getOption[T](option: Option[T]): T = option.get
-  val window=driver.navigateTo("http://www.bing.com")
-  window.findElement("id", "sb_form_q").sendKeys("Juno mission")
-  window.findElement("id","sb_form").submit()
-  window.executeJS("console.log('testJS')")
-
-  scala.io.StdIn.readLine("press any to shut down the client.....")
-  WebDriverClient.shutdownClient()
   //WebDriverClient.shutdownServer(host) //when necessary
 }
