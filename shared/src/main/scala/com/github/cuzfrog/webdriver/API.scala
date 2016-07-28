5dfc590bf6ee627069a89c1b2f349a559fe732b4
Cause chung <cuzfrog@139.com>
2016-07-28 08:40:24 +0800

    add implicit wait time

--- shared/src/main/scala/com/github/cuzfrog/webdriver/API.scala
+++ shared/src/main/scala/com/github/cuzfrog/webdriver/API.scala
@@ -1,13 +1,15 @@
 package com.github.cuzfrog.webdriver
+import com.github.cuzfrog.webdriver.DriverTypes.DriverType
 
 /**
   * Shared Api.
   */
 private[webdriver] trait Api {
-  def newDriver(name: String, typ: DriverTypes.DriverType): Driver
+  def newDriver(name: String, typ: DriverType, waitSec: Int = 10): Driver
   def retrieveDriver(name: String): Option[Driver]
-  def findElement(id: Long, attr: String, value: String): Element
-  def findElements(id: Long, attr: String, value: String): Seq[Element]
+  def findElement(webBody: WebBody, attr: String, value: String): Element
+  def findElements(webBody: WebBody, attr: String, value: String): Seq[Element]
+  def executeJS(webBody: WebBody, script: String): Any
   def sendKeys(element: Element, keys: String): Unit
   def submit(element: Element): Unit
   def click(element: Element): Unit
